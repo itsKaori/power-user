08c64a6cb00d6f3a1bef5f6b4ffbec29c88a0267
itsKaori <kaorihall@ufl.edu>
2023-05-01 18:03:57 -0400

    i just want it to work please

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// app.use(express.static('static'));
+
+app.listen(PORT, () => {
+  console.log('Server is listening on port ${PORT}');
+});
