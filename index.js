caf9c8d9b897b30631135eb65d3f9236fd7f5a1a
itsKaori <kaorihall@ufl.edu>
2023-05-01 18:17:58 -0400

    reorganizing to get changes updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './app';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -8,13 +8,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-// app.use(express.static('static'));
-
-app.listen(PORT, () => {
-  console.log('Server is listening on port ${PORT}');
-});
